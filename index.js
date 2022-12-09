e37fdbeb5e01d77c9d43b1325f2e1a04161622f9
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-09 13:24:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+let name = "name";
+let email = "email";
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+  .then((response) => {
+    return response.json()
+})
+  .then((userData) => {
+    document.querySelector('body').innerHTML = userData.id
+  })
+  .catch((error) => {
+    document.querySelector('body').innerHTML = error.message
+  })
+}
+submitData()
